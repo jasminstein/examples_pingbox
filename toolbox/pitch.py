9cfecc0e1798147a69a94067eebcbe535ddf0415
jasminstein <jasmin.stein@outlook.de>
2026-05-06 13:31:14 +0200

    commit initial examples

--- toolbox/pitch.py
+++ toolbox/pitch.py
@@ -36,4 +36,37 @@ def erb_rate_to_hz(erb_rate):
 
     f = (10 ** (erb_rate / 21.4) - 1) * (1000 / 4.37)  
 
-    return f  
+    return f
+
+def map_arbitrary_scale_erb(x_in, x_min=-1, x_max=1, f_min=500, f_max=1300):
+
+    '''
+    Maps an experimental frequency range (f_min to f_max) to ERB, linearly interpolates for a given range, and maps to Hz
+    --> aim: equal steps in x_in result in PERCEPTUALLY equal steps on Hz scale
+
+    Args:
+        x_in: arbitrary value between x_min and x_max
+        x_min: minimum value on arbitrary scale (default = -1)
+        x_max: maximum value on arbitrary scale (default = 1)
+        f_min: minimum f used in experiment in Hz (default = 500 Hz; based on Jasmin's experimental setup)
+        f_max: maximum f used in experiment in Hz (default = 1300 Hz; based on Jasmin's experimental setup)
+
+    Returns:
+        f: frequency corresponding to x_in in Hz
+    '''
+    
+    
+    if np.any(x_in < x_min) or np.any(x_in > x_max):
+        raise ValueError(
+            f"x_input must be within [{x_min}, {x_max}], "
+            f"but got {x_input}"
+        )
+
+    e_min = hz_to_erb_rate(f_min)
+    e_max = hz_to_erb_rate(f_max)
+
+    e = e_min + (e_max - e_min) * (x_in - (x_min)) / (x_max - (x_min))
+
+    f = (10 ** (e / 21.4) - 1) * (1000 / 4.37)       
+
+    return f   
